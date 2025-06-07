be81befb99295d6e514c3a30bff9691fb79ed4e4
Lee Kaburu <leekaburu@gmail.com>
2025-06-07 16:04:05 +0300

    Maps urls onto the app

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'), # Creates a new user
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # give back JWT tokens
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # Update access token
+    path('register/', RegisterView.as_view(), name='register'), # Registers a new user
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # give back access and refresh JWT tokens
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # Update access token when given the right refresh token
 ]
