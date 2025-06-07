ce16df375b5ff14fb3a0e15fb74acc2b9d5b0ec1
Lee Kaburu <leekaburu@gmail.com>
2025-06-07 14:51:18 +0300

    Created an app Jobs that'll handle job postings

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
