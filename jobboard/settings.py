4b79d86f6c90e3991cf604d23e03b1da804e6d12
Lee Kaburu <leekaburu@gmail.com>
2025-06-07 16:28:56 +0300

    Made all views protected by default

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -57,6 +57,9 @@ REST_FRAMEWORK = {
     # We’ll configure authentication classes in a bit
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
