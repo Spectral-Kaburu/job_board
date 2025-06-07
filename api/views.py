c36dfc907415c3928f50038761a180ee537fe16c
Lee Kaburu <leekaburu@gmail.com>
2025-06-07 15:59:03 +0300

    This view handles user registration

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics # Has pre-built views for CRUD
 from .serializers import RegisterSerializer
 from users.models import User
 from rest_framework.permissions import AllowAny
