ea79b5cbab34198a50e8cee32e4a8bfbf5098a96
Lee Kaburu <leekaburu@gmail.com>
2025-06-15 14:48:08 +0300

    Added resume and cover letter to the model and allowed them to be blank

--- applications/models.py
+++ applications/models.py
@@ -6,7 +6,10 @@ from jobs.models import Job
 
 class Application(models.Model):
     applicant = models.ForeignKey(User, on_delete=models.CASCADE)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    resume = models.FileField(upload_to='resumes/')
-    cover_letter = models.TextField()
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
+    resume = models.FileField(upload_to='resumes/', blank=True, null=True)
+    cover_letter = models.TextField(blank=True, null=True)
     applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.applicant.email} applied to {self.job.title}."
