df0bd6a8ad4c9673799061ffd60c4d1da90597b7
Lee Kaburu <leekaburu@gmail.com>
2025-06-15 15:06:27 +0300

    Added the urls from the other apps, created a documentaion andintegrated the media url into the system

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -14,10 +14,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Job Board API",
+        default_version='v1',
+        description="API documentaionfor the Job Board",
+        contact=openapi.Contact(email="support@example.com"),
+        ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('jobs/', include('jobs.urls'))
+    path('api/jobs/', include('jobs.urls')),
+    path('api/applications/', include('applications.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),      # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),     # refresh
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
