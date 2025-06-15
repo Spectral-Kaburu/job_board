aa3dfbaf850db1320f28f5fdc450ba21c3df1139
Lee Kaburu <leekaburu@gmail.com>
2025-06-15 15:04:23 +0300

    Created functions to: Create jobs, check if recruiter and list jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import BasePermission
+from rest_framework import generics
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+
+class IsRecruiter(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_recruiter
+
+class JobListCreateView(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsRecruiter]
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_recruiter:  # type: ignore
+            raise PermissionDenied("Only recruiters can post jobs.")
+        serializer.save(posted_by=self.request.user)
+
+class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def my_protected_view(request):
+    return Response({"message": f"Hello {request.user.first_name}, you are authenticated"})
+
+
